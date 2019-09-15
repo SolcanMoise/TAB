35f24a71d58d27a0e0a8589796055694afa255fe
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-09-15 15:48:28 +0300

    Make server respond with .html file

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('index.html', {root: '.'}))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`TAB app listening on port ${port}!`))
