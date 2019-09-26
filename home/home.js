32dc5023d32d8fd32882aa2629f7d20bc315bbc6
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-09-26 21:25:33 +0300

    Template for home page

---
+++ home/home.js
@@ -0,0 +1,16 @@
+angular.module('TAB')
+    .controller('HomeController', function($scope) {
+        $scope.title1 = 'Title1';
+        $scope.description1 = 'Description1';
+        $scope.content1 = 'Content1';
+
+        $scope.title2 = 'Title2';
+        $scope.description2 = 'Description2';
+        $scope.content2 = 'Content2';
+
+        $scope.title3 = 'Title3';
+        $scope.description3 = 'Description3';
+        $scope.content3 = 'Content3';
+
+    });
+
